73b894aaaf629c062ee9daaae404ac45d3ec266e
pwings <pwings@controlid.multiexportfoods.com>
2016-03-14 17:06:17 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+/*jslint node:true*/
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var oracledb = require('oracledb');
+
+
+// Use body parser to parse JSON body
+app.use(bodyParser.json());
+
+var connAttrs = {
+    "user": "user_rrhh",
+    "password": "marcas",
+    "connectString": "DES_ORACLE11_RRHH"
+}
+
+// Http method: GET
+// URI        : /employee/:RUT
+app.get('/employee/:RUT', function (req, res) {
+    "use strict";
+
+    oracledb.getConnection(connAttrs, function (err, connection) {
+        if (err) {
+            // Error connecting to DB
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(JSON.stringify({
+                status: 500,
+                message: "Error connecting to Oracle",
+                detailed_message: err.message
+            }));
+            return;
+        };
+        connection.execute("select marcasmef.fn_sp_es_empleado(:RUT) from dual", [req.params.RUT], {
+            outFormat: oracledb.OBJECT // Return the result as Object
+        }, function (err, result) {
+            if (err || result.rows.length < 1) {
+                res.set('Content-Type', 'application/json');
+                var status = err ? 500 : 404;
+                res.status(status).send(JSON.stringify({
+                    status: status,
+                    message: err ? "Error getting the employee status" : "Employee doesn't exist",
+                    detailed_message: err ? err.message : ""
+                }));
+            } else {
+                res.contentType('application/json').status(200).send(JSON.stringify(result.rows));
+            }
+            // Release the connection
+            connection.release(
+                function (err) {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        console.log("GET /employee/" + req.params.RUT + " : Connection released");
+                    }
+                });
+        });
+    });
+});
+
+var server = app.listen(3000, function () {
+    "use strict";
+
+    var host = server.address().address,
+        port = server.address().port;
+
+    console.log(' Server is listening at http://%s:%s', host, port);
+});
